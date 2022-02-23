219c14fab98bd90e64265245cf7a1b7c97f60c66
Syreen <sbanabilah@gmail.com>
2022-02-23 18:55:02 -0500

    change the name of app

--- HW12/Shop/models/StuffModel.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-let stuffSchema = mongoose.Schema({name: String, order: [[String]]});
-// Singular name of collection, pluralized in DB
-let collectionName = 'orders';
-let StuffModel = mongoose.model(collectionName,
-                                stuffSchema);
-module.exports = StuffModel;
